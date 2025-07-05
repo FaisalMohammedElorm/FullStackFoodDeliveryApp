6cf210aa8e5b86cca87485f0d4fbae3ad094a544
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-07-05 23:58:37 +0000

    tailwind along with assets

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 const Index = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl font-bold text-blue-500">
+      <Text className="text-4xl font-bold text-red-800">
         Welcome to Nativewind!
       </Text>
     </View>
