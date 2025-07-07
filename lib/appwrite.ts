eca65db9f78755b475917600f2423dd69c250c8b
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-07-07 22:54:52 +0000

    Appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.jsm.fooddelivery",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+}
