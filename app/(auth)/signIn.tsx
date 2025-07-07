27246510b29acaa46692ffc9bbdff87dd9bc1947
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-07-07 22:45:27 +0000

    Appwrite setup

--- app/(auth)/signIn.tsx
+++ app/(auth)/signIn.tsx
@@ -1,6 +1,6 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
-import signUp from './signUp';
+// import signUp from './signUp';
 import { router } from 'expo-router';
 
 const signIn = () => {
