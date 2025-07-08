4ef7298e1c24a46bd88d50f7c271a95487c86d59
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-07-08 00:35:40 +0000

    Auth Screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { images } from '@/constants'
 import { Slot } from 'expo-router'
+import React from 'react'
+import { Dimensions, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View, Image } from 'react-native'
 
 const _layout = () => {
   return (
-    <View>
-      <Text>Auth Layout</Text>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView className='bg-white h-full' keyboardShouldPersistTaps='handled'>
+        <View className='w-full relative' style={{ height: Dimensions.get('screen').height / 2.25}}>
+          <ImageBackground 
+            source={images.loginGraphic}
+            className='size-full rounded-b-lg'
+            resizeMode='stretch'
+          />
+          <Image 
+            source={images.logo}
+            className='self-center size-40 absolute -bottom-16 z-10'
+          />
+        </View>
+      </ScrollView>
       <Slot />
-    </View>
+    </KeyboardAvoidingView>
   )
 }
 
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.jsm.fooddelivery",
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "686c612f00377b83937f",
+  userCollectionId: "686c6185002cc618af14",
 }
